9789866f24869503592799a2470ed95690c5b160
David Schmitt <david.schmitt@puppet.com>
2020-05-18 09:40:30 +0100

    (maint) Remove diagnostics-hiding from Pester wrapper

--- src/tests/pester.ps1
+++ src/tests/pester.ps1
@@ -1,13 +1,13 @@
 param (
   $TestGeneral = $true,
-  
+
   $TestFunctions = $true,
-  
+
   [ValidateSet('None', 'Default', 'Passed', 'Failed', 'Pending', 'Skipped', 'Inconclusive', 'Describe', 'Context', 'Summary', 'Header', 'Fails', 'All')]
-  $Show = "None",
-  
+  $Show = ('Summary', 'Failed'),
+
   $Include = "*",
-  
+
   $Exclude = ""
 )
 
@@ -19,9 +19,6 @@ Remove-Module puppet.dsc -ErrorAction Ignore
 Import-Module "$PSScriptRoot\..\puppet.dsc.psd1" -Force
 
 $totalFailed = 0
-$totalRun = 0
-
-$testresults = @()
 
 #region Run General Tests
 if ($TestGeneral)
@@ -30,21 +27,10 @@ if ($TestGeneral)
   foreach ($file in (Get-ChildItem "$PSScriptRoot\general" | Where-Object Name -like "*.Tests.ps1"))
   {
     Write-PSFMessage -Level Significant -Message "  Executing <c='em'>$($file.Name)</c>"
-    $results = Invoke-Pester -Script $file.FullName -Show $Show -PassThru
+    Invoke-Pester -Script $file.FullName -Show $Show
     foreach ($result in $results)
     {
-      $totalRun += $result.TotalCount
       $totalFailed += $result.FailedCount
-      $result.TestResult | Where-Object { -not $_.Passed } | ForEach-Object {
-        $name = $_.Name
-        $testresults += [pscustomobject]@{
-          Describe = $_.Describe
-          Context  = $_.Context
-          Name	 = "It $name"
-          Result   = $_.Result
-          Message  = $_.FailureMessage
-        }
-      }
     }
   }
 }
@@ -53,39 +39,23 @@ if ($TestGeneral)
 #region Test Commands
 if ($TestFunctions)
 {
-Write-PSFMessage -Level Important -Message "Proceeding with individual tests"
+  Write-PSFMessage -Level Important -Message "Proceeding with individual tests"
   foreach ($file in (Get-ChildItem "$PSScriptRoot\functions" -Recurse -File | Where-Object Name -like "*Tests.ps1"))
   {
     if ($file.Name -notlike $Include) { continue }
     if ($file.Name -like $Exclude) { continue }
-    
-    Write-PSFMessage -Level Significant -Message "  Executing $($file.Name)"
+
+    Write-PSFMessage -Level Significant -Message "  Executing <c='em'>$($file.Name)</c>"
     $results = Invoke-Pester -Script $file.FullName -Show $Show -PassThru
     foreach ($result in $results)
     {
-      $totalRun += $result.TotalCount
       $totalFailed += $result.FailedCount
-      $result.TestResult | Where-Object { -not $_.Passed } | ForEach-Object {
-        $name = $_.Name
-        $testresults += [pscustomobject]@{
-          Describe = $_.Describe
-          Context  = $_.Context
-          Name	 = "It $name"
-          Result   = $_.Result
-          Message  = $_.FailureMessage
-        }
-      }
     }
   }
 }
 #endregion Test Commands
 
-$testresults | Sort-Object Describe, Context, Name, Result, Message | Format-List
-
-if ($totalFailed -eq 0) { Write-PSFMessage -Level Critical -Message "All <c='em'>$totalRun</c> tests executed without a single failure!" }
-else { Write-PSFMessage -Level Critical -Message "<c='em'>$totalFailed tests</c> out of <c='sub'>$totalRun</c> tests failed!" }
-
 if ($totalFailed -gt 0)
 {
-  throw "$totalFailed / $totalRun tests failed!"
+  throw "$totalFailed tests failed!"
 }
